8b131f5b2f1d360a3649834815daa2dea38bc4c1
jfsa-lo <jfsuaza92@gmail.com>
2020-03-20 04:43:45 -0500

    Heroku de aqui en adelante

--- SGT/settings.py
+++ SGT/settings.py
@@ -136,7 +136,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media config
